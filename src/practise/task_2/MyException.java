5c0c3b38e039306dbc1ec5539f597dd9c95651fc
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-10-21 20:59:12 +0300

    task_2

--- src/practise/task_2/MyException.java
+++ src/practise/task_2/MyException.java
@@ -1,5 +1,41 @@
 package practise.task_2;
 
 public class MyException extends  Exception{
-    
+    public MyException(String message){
+        super(message);
+    }
+}
+
+class Animal {
+    private boolean collar;
+    private boolean leash;
+    public boolean isCollar() throws MyException {
+        return collar;
+    }
+    public void setCollar(boolean collar) {
+        this.collar = collar;
+    }
+    public boolean isLeash() throws MyException {
+        return leash;
+    }
+    public void setLeash(boolean leash) {
+        this.leash = leash;
+    }
+    void walking(){
+        System.out.println("Выгул");
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        try{
+            if (!dog.isCollar())
+            throw new MyException("Не вся экипировка");
+            if (!dog.isLeash())
+                throw new MyException("Не вся экипировка");
+            dog.walking();
+        }catch (MyException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
